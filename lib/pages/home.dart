2696bf6a9cf1aed2950a79ae39623a0236e77ad0
unknown <melanyrm2507@gmail.com>
2024-11-02 18:50:06 -0400

    Iniciando app de geolocalizacion

---
+++ lib/pages/home.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("mapas"),
+        ),
+        body: FlutterMap(
+          options: MapOptions(
+            initialCenter: LatLng(-16.65288074613133,
+                -68.30169158902014), // Center the map over London
+            initialZoom: 9.2,
+          ),
+          children: [
+            TileLayer(
+              // Display map tiles from any source
+              urlTemplate:
+                  'https://tile.openstreetmap.org/{z}/{x}/{y}.png', // OSMF's Tile Server
+              userAgentPackageName: 'com.example.app',
+              // And many more recommended properties!
+            ),
+            RichAttributionWidget(
+              // Include a stylish prebuilt attribution widget that meets all requirments
+              attributions: [
+                TextSourceAttribution(
+                  'OpenStreetMap contributors',
+                  onTap: () => launchUrl(Uri.parse(
+                      'https://openstreetmap.org/copyright')), // (external)
+                ),
+                // Also add images...
+              ],
+            ),
+          ],
+        ));
+  }
+}
